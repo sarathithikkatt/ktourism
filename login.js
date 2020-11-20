98fcf9328d22b20ca17fae24a8dedde4eb136957
I S Sarath <sarath2000is@gmail.com>
2020-11-20 19:13:42 +0530

    section 4 fin

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ let error = document.getElementById("error");
 
 function loginvalid() {
     let emailexp = /^([\w\.-]+)@([\w\.-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
-    let passexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$/
+    let passexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,}$/
     if (emailexp.test(email.value)) {
         if (passexp.test(pass.value)) {
             return true
--- signup.js
+++ signup.js
@@ -6,10 +6,13 @@ let pass1 = document.getElementById("inputPassword");
 let pass2 = document.getElementById("inputPassword2")
 let passerror = document.getElementById("errorpass")
 let error = document.getElementById("error");
+let strenght = document.getElementById("strenght");
 var phoneb = false;
 var mailb = false;
 var pass1b = false;
 var pass2b = false;
+var str = ["Bad", "Meh..", "Good"]
+var color = ["Red", "Yellow", "Green"];
 
 function phonevalid() {
     let phoneexp = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
@@ -27,13 +30,42 @@ function phonevalid() {
 }
 
 function passstrenght() {
-    let passexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$/
-    if (passexp.test(pass1.value)) {
-        pass1b = true;
+    var passstrenght = 0;
+    let passexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,}$/
+        // morethan8 1
+    let str1 = /^(?=.{8,})$/
+        //lowercase&upercase 2
+    let str2 = /^(?=.*[a-z])(?=.*[A-Z])$/
+        // Numeric_Char 3
+    let str3 = /^(?=.*[0-9])(?=.*[0-9])$/
+        // spclchar 4
+    let str4 = /^(?=.*[!@#$%^&*])$/
+    if (str4.test(pass1.value)) {
+        passstrenght = (passstrenght + 1) % 3;
+        strenght.innerHTML = str[passstrenght];
+        strenght.style.color = color[passstrenght];
+        return true;
+    } else if (str3.test(pass1.value)) {
+        passstrenght = (passstrenght + 1) % 3;
+        strenght.innerHTML = str[passstrenght];
+        strenght.style.color = color[passstrenght];
+        return true;
+    } else if (str2.test(pass1.value)) {
+        passstrenght = (passstrenght + 1) % 3;
+        strenght.innerHTML = str[passstrenght];
+        strenght.style.color = color[passstrenght];
+        return true;
+    } else if (passexp.test(pass1.value)) {
+        passstrenght = 2;
+        strenght.innerHTML = str[passstrenght];
+        strenght.style.color = color[passstrenght];
+        return true;
     } else {
         pass1b = false;
+        strenght.innerHTML = str[passstrenght];
+        strenght.style.color = color[passstrenght];
+        return false;
     }
-
 }
 
 function checkpass() {
@@ -42,11 +74,13 @@ function checkpass() {
         passerror.innerHTML = "passwords match";
         passerror.style.color = "green";
         pass2b = true;
+        return true;
     } else {
         pass2.style.color = "red";
         passerror.innerHTML = "passwords doesn't match";
         passerror.style.color = "red";
         pas2b = false;
+        return true;
     }
 }
 
